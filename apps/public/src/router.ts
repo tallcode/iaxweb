7aff4db0a0cd584369daf27b023974edf0ac795b
tallcode <1323833+tallcode@users.noreply.github.com>
2026-08-15 17:22:48 +0800

    Lazy load public pages

--- apps/public/src/router.ts
+++ apps/public/src/router.ts
@@ -1,11 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import MapPage from './pages/MapPage.vue'
-import TopologyPage from './pages/TopologyPage.vue'
 
 export const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { alias: '/', component: TopologyPage, path: '/topology' },
-    { component: MapPage, path: '/map' },
+    { alias: '/', component: () => import('./pages/TopologyPage.vue'), path: '/topology' },
+    { component: () => import('./pages/MapPage.vue'), path: '/map' },
   ],
 })
